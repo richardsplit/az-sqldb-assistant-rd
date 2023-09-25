5bc36f944ded12e57ee95065ff68eec3c58a9525
Richard Georgiev <richard_bg@abv.bg>
2023-09-25 19:52:59 +0300

    dasd

--- search_and_chat.py
+++ search_and_chat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from azure.core.credentials import AzureKeyCredential
 from azure.search.documents import SearchClient
-from azure_openai import get_completion_from_messages_usr  
+from azure_openai import get_completion_from_messages_usr
 
 AZURE_SEARCH_URL = "https://srch-smartresearch-sb.search.windows.net"
 AZURE_SEARCH_INDEX = "aclcompanylistone"
@@ -11,16 +11,23 @@ search_client = SearchClient(AZURE_SEARCH_URL, AZURE_SEARCH_INDEX, AzureKeyCrede
 
 def run_search_and_chat():
     st.title("Chat and Search Assistant")
-
     user_input = st.text_input("Enter your message or search query:")
+    
     if user_input:
-        if "search:" in user_input.lower():  # perform a search
+        if "search:" in user_input.lower():  # Perform a search
             search_query = user_input.replace("search:", "").strip()
             results = search_client.search(search_query)
+            
             st.write("Search Results:")
             for result in results:
-                st.write(result)
-        else:  
+                st.write(f"ID: {result['id']}")
+                st.write(f"Company Name: {result['company_name']}")
+                st.write(f"Marketing Class Description: {result['marketingClass_Description']}")
+                st.write(f"Net Income: {result['netIncome']}")
+                st.write(f"Market Cap: {result['marketCap']}")
+                st.write("---")
+                
+        else:  # Interact with OpenAI
             response = get_completion_from_messages_usr(user_input)
             st.write("Response:")
             st.write(response)
